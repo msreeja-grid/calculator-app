f87cce387e10fcea77f17a15e66a9450d3b4f935
Sreeja Madala <msreeja@griddynamics.com>
2026-01-27 20:27:58 +0530

    Initial calculator app with FastAPI Frontend  and Backend

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+app=FastAPI(title="calculator API")
+class Numbers(BaseModel):
+    a: float
+    b: float
+@app.get("/")
+def root():
+    return {"message": "Welcome to the calculator API!"}
+@app.post("/add")
+def add(numbers: Numbers):
+    return {"result": numbers.a + numbers.b}
+@app.post("/subtract")
+def subtract(numbers: Numbers):
+    return {"result": numbers.a - numbers.b}
+@app.post("/multiply")
+def multiply(numbers: Numbers):
+    return {"result": numbers.a * numbers.b}
+@app.post("/divide")
+def divide(numbers: Numbers):
+    if numbers.b == 0:
+        return {"error": "Division by zero is not allowed."}
+    return {"result": numbers.a / numbers.b}
+    
